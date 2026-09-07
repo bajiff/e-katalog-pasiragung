44c9c35f1aa055f5a390fa2becc4b0269ecabbc7
bajiff <bagusajifernando@gmail.com>
2026-09-07 15:45:30 +0700

    feat(structure): add barrel export index files for pages and routes

---
+++ src/pages/admin/index.js
@@ -0,0 +1,6 @@
+export { default as DashboardPage } from './DashboardPage'
+export { default as ProductsPage } from './ProductsPage'
+export { default as CategoriesPage } from './CategoriesPage'
+export { default as OwnersPage } from './OwnersPage'
+export { default as ProfilePage } from './ProfilePage'
+export { default as UsersPage } from './UsersPage'
---
+++ src/pages/public/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home'
+export { default as ProductDetail } from './ProductDetail'
+export { default as OwnerProfile } from './OwnerProfile'
+export { default as Login } from './Login'
+export { default as Register } from './Register'
